9269fa7c1c74add87caf578623c50c9afebf9158
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-12 00:02:46 +0530

    update

--- cal/sample.js
+++ cal/sample.js
@@ -1,5 +1,7 @@
 
-var no1Sel,no2Sel;
+// var no1Sel,no2Sel;
+var calArr = [];
+var count = 0;
 
 function myFun(res){
 
@@ -7,87 +9,114 @@ function myFun(res){
     var x,y;
     
 
-    if(res == 'no1'){
-        no1Sel = true;
-        no2Sel = false;
-    }
-    else if(res == 'no2'){
-        no2Sel = true;
-        no1Sel = false;
-    }
+    // if(res == 'no1'){
+    //     no1Sel = true;
+    //     no2Sel = false;
+    // }
+    // else if(res == 'no2'){
+    //     no2Sel = true;
+    //     no1Sel = false;
+    // }
 
-    if(no1Sel){
-        x = document.getElementById(res).value;
-        document.getElementById('no1').value += x;
+    // if(no1Sel){
+    //     x = document.getElementById(res).value;
+    //     document.getElementById('no1').value += x;
 
-        console.log(x);
-    }
+    //     console.log(x);
+    // }
 
-    if(no2Sel){
-        y = document.getElementById(res).value;
-        document.getElementById('no2').value += y;
+    // if(no2Sel){
+    //     y = document.getElementById(res).value;
+    //     document.getElementById('no2').value += y;
 
-        console.log(x);
-    }
+    //     console.log(x);
+    // }
 
-}
-
-function add(){
-
-    var no1,no2,result;
-
-    no1 = document.getElementById('no1').value;
-    no2 = document.getElementById('no2').value;
-
-    result = parseInt(no1) + parseInt(no2);
-
-    document.getElementById('res').value = result;
-    console.log("add");
-
+    x = document.getElementById(res).value;
+    document.getElementById('res').value += x;
+    document.getElementById('oprt').innerHTML += x;
+    calArr.push(x);
+    console.log(calArr);
 
 }
 
-function sub(){
+function add(sign){
 
-    var no1,no2,result;
+    // var no1,no2,result;
 
-    no1 = document.getElementById('no1').value;
-    no2 = document.getElementById('no2').value;
+    var oprt = document.getElementById(sign).value;
+    document.getElementById('oprt').innerHTML += oprt;
+    calArr.push(oprt);
+    document.getElementById('res').value = "";
 
-    result = parseInt(no1) - parseInt(no2);
 
-    document.getElementById('res').value = result;
-    console.log("sub");
+    // no1 = document.getElementById('no1').value; 
+    // no2 = document.getElementById('no2').value;
+
+    // result = parseInt(no1) + parseInt(no2);
+
+    // document.getElementById('res').value = result;
+    // console.log("add");
 
 
 }
 
-function mul(){
+function sub(sign){
 
-    var no1,no2,result;
+    // var no1,no2,result;
 
-    no1 = document.getElementById('no1').value;
-    no2 = document.getElementById('no2').value;
+    // no1 = document.getElementById('no1').value;
+    // no2 = document.getElementById('no2').value;
 
-    result = parseInt(no1) * parseInt(no2);
+    // result = parseInt(no1) - parseInt(no2);
 
-    document.getElementById('res').value = result;
-    console.log("mul");
+    // document.getElementById('res').value = result;
+    // console.log("sub");
+
+    var oprt = document.getElementById(sign).value;
+    document.getElementById('oprt').innerHTML += oprt;
+    calArr.push(oprt);
+    document.getElementById('res').value = "";
 
 
 }
 
-function div(){
+function mul(sign){
 
-    var no1,no2,result;
+    // var no1,no2,result;
 
-    no1 = document.getElementById('no1').value;
-    no2 = document.getElementById('no2').value;
+    // no1 = document.getElementById('no1').value;
+    // no2 = document.getElementById('no2').value;
 
-    result = parseInt(no1) / parseInt(no2);
+    // result = parseInt(no1) * parseInt(no2);
 
-    document.getElementById('res').value = result;
-    console.log("div");
+    // document.getElementById('res').value = result;
+    // console.log("mul");
+
+    var oprt = document.getElementById(sign).value;
+    document.getElementById('oprt').innerHTML += oprt;
+    calArr.push(oprt);
+    document.getElementById('res').value = "";
+
+
+}
+
+function div(sign){
+
+    // var no1,no2,result;
+
+    // no1 = document.getElementById('no1').value;
+    // no2 = document.getElementById('no2').value;
+
+    // result = parseInt(no1) / parseInt(no2);
+
+    // document.getElementById('res').value = result;
+    // console.log("div");
+
+    var oprt = document.getElementById(sign).value;
+    document.getElementById('oprt').innerHTML += oprt;
+    calArr.push(oprt);
+    document.getElementById('res').value = "";
 
 
 }
@@ -95,13 +124,31 @@ function div(){
 function clr(){
     console.log("clear");
    
-    var no1 = document.getElementById('no1');
-    var no2 =  document.getElementById('no2');
+    // var no1 = document.getElementById('no1');
+    // var no2 =  document.getElementById('no2');
     var res =  document.getElementById('res');
+    var oprt = document.getElementById('oprt');
 
-    no1.value = "";
-    no2.value = "";
+    // no1.value = "";
+    // no2.value = "";
     res.value = "";
+    oprt.innerHTML = "";
+    calArr = [];
+
+
+}
+
+function eq(){
+
+    var sum = 0;    
+
+    for(var i=0; i<calArr.length;i++){
+        sum += calArr[i];
+    }
+
+    document.getElementById('res').value = eval(sum);
+
+
 
 
 }
